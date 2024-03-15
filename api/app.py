8729c9427a0d7b1172d52359b4bb817ad1497e91
Allaine Tan <allainericci@gmail.com>
2024-03-15 21:17:24 +0800

    refactor: decompose to smaller functions

--- api/app.py
+++ api/app.py
@@ -25,36 +25,33 @@ def hello():
 # Prediction endpoint
 @app.route('/predict', methods=['POST'])
 @cross_origin(allow_headers=['Content-Type'])
-def upload():
+def predict():
     if request.method == 'POST':
-        # Access the uploaded file
-        filestr = request.files['file'].read()  # Read uploaded file from request
-        file_bytes = np.frombuffer(filestr, np.uint8)  # Convert to numpy array
-        face = cv2.imdecode(file_bytes, cv2.IMREAD_UNCHANGED)  # Decode image
-
-        # Preprocess the image
-        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-        face = cv2.resize(face, (224, 224))
-        face = np.expand_dims(face, axis=0)
-        face = (face.astype(np.float32) - 127.5) / 127.5  # Normalize the image
-
-        # Perform inference
-        interpreter.set_tensor(interpreter.get_input_details()[0]['index'], face)
-        interpreter.invoke()
-        output = interpreter.get_tensor(interpreter.get_output_details()[0]['index'])
-        
-        # Interpret the prediction
-        output_class = np.argmax(output)
-        predicted_class = mask_label[output_class]
-        confidence = float(np.max(output))
-
-        # Return the result
-        return {
-            'class': predicted_class,
-            'confidence': confidence
-        }
-
+        file = request.files['file']
+        result = process_image(file)
+        return result
     return None
+
+def process_image(file):
+    filestr = file.read()  # Read uploaded file from request
+    file_bytes = np.frombuffer(filestr, np.uint8)  # Convert to numpy array
+    face = cv2.imdecode(file_bytes, cv2.IMREAD_UNCHANGED)  # Decode image
+    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+    face = cv2.resize(face, (224, 224))
+    face = np.expand_dims(face, axis=0)
+    face = (face.astype(np.float32) - 127.5) / 127.5  # Normalize the image
+    return perform_inference(face)
+
+def perform_inference(face):
+    # Perform inference
+    interpreter.set_tensor(interpreter.get_input_details()[0]['index'], face)
+    interpreter.invoke()
+    output = interpreter.get_tensor(interpreter.get_output_details()[0]['index'])
+    # Interpret the prediction
+    output_class = np.argmax(output)
+    predicted_class = mask_label[output_class]
+    confidence = float(np.max(output))
+    return {'class': predicted_class, 'confidence': confidence}
 
 if __name__ == '__main__':
     app.run(debug=True)
